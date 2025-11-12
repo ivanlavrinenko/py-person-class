fdb8eab2c8270ff7eeab843aeac9cef61d48d256
Ivan Lavrinenko <lavrinenkoivan51@gmail.com>
2025-11-12 18:20:58 +0100

    Person class created, create_person_list method implemented

--- app/main.py
+++ app/main.py
@@ -1,8 +1,23 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    result = []
+    for person in people:
+        person_instance = Person(person.get("name"), person.get("age"))
+        result.append(person_instance)
+        Person.people[person_instance.name] = person_instance
+
+    for person in people:
+        if person.get("wife") is not None:
+            Person.people.get(person.get("name")).wife\
+                = Person.people.get(person.get("wife"))
+        if person.get("husband") is not None:
+            Person.people.get(person.get("name")).husband\
+                = Person.people.get(person.get("husband"))
+    return result
